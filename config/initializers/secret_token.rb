436653effae7e9f2c7d0d7fbc48b1f6c5afbe438
etienne araya <etienne.araya@geoxite.com>
2011-12-08 17:55:30 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e0b19598af9c84ce84c0d33ab3e3554881ed91abd2929d90115688242f0efa47d09983e1eb72f60a29edf81fae60c562fb05c1257c9568a10a4205693f0c2466'
